8fee16a7bf531b28868315e8b62975f85bd8185b
Aleksandr Lavrov <sancho2140@gmail.com>
2024-04-18 11:26:12 +0300

    first draft

---
+++ colors.js
@@ -0,0 +1,75 @@
+export const colors = [
+    {
+        color2: "#43ff82",
+        color1: "#009e34",
+        action: 'X',
+        value: '2'
+    },
+    {
+        color2: "#44ffca",
+        color1: "#008f65",
+        action: '+',
+        value: '500'
+    },
+    {
+        color2: "#45ffff",
+        color1: "#00cdcd",
+        action: '-',
+        value: '5000'
+    },
+    {
+        color2: "#4e8cff",
+        color1: "#0739aa",
+        action: '/',
+        value: '4'
+    },
+    {
+        color2: "#964bfc",
+        color1: "#4206a0",
+        action: '+',
+        value: '1500'
+    },
+    {
+        color2: "#d945ff",
+        color1: "#75019b",
+        action: '/',
+        value: '2'
+    },
+    {
+        color2: "#ff47d1",
+        color1: "#98016c",
+        action: 'X',
+        value: '1'
+    },
+    {
+        color2: "#ff4646",
+        color1: "#c60000",
+        action: '-',
+        value: '500'
+    },
+    {
+        color2: "#ffaa47",
+        color1: "#b95104",
+        action: '+',
+        value: '600'
+    },
+    {
+        color2: "#ffdc48",
+        color1: "#b97704",
+        action: '/',
+        value: '3'
+    },
+    {
+        color2: "#ffff4a",
+        color1: "#c1a005",
+        action: '-',
+        value: '900'
+    },
+    {
+        color2: "#e7ff69",
+        color1: "#7b971b",
+        action: '-',
+        value: '3000'
+    }
+]
+
---
+++ script.js
@@ -0,0 +1,151 @@
+import { colors } from './colors.js'
+
+const playBtn = document.querySelector('.center');
+const wheelBody = document.querySelector(".wheel");
+const arrow = document.querySelector('.arrow');
+const gameScore = document.querySelector('.game-score');
+const bet = document.querySelector('#bet');
+const popUp = document.querySelector('.bet-popup');
+const errorPopUp = document.querySelector('.error-message');
+
+let savedDegree;
+let newBet;
+
+for (let i = 0; i < colors.length; i++) {
+    gameScore.textContent = 0;
+
+    const triangle = document.createElement('div');
+    const label = document.createElement('span')
+    label.classList.add('label');
+    label.textContent = `${colors[i].action} ${colors[i].value}`;
+    triangle.appendChild(label);
+
+
+    triangle.classList.add('triangle');
+    triangle.id = `${i}`;
+    triangle.setAttribute('action', `${colors[i].action}`);
+    triangle.setAttribute('value', `${colors[i].value}`);
+
+
+    triangle.style.backgroundImage = `linear-gradient(${colors[i].color1}, ${colors[i].color2})`
+
+    triangle.style.transform = `translateX(-50%) rotate(${30 * i}deg)`
+
+    wheelBody.appendChild(triangle);
+}
+
+playBtn.addEventListener('click', play)
+
+bet.addEventListener('input', () => {
+    bet.style.border = '2px solid black';
+    errorPopUp.classList.remove('goUp');
+})
+
+function play() {
+    popUp.classList.remove('move-pop-up');
+
+    try {
+        inputValidation(bet.value);
+
+        newBet = parseInt(bet.value);
+        bet.value = '';
+
+        const rotateDeg = 360 * 3 + Math.floor(Math.random() * 670);
+
+        wheelBody.style.transition = 'transform 5s';
+        wheelBody.classList.add('blur');
+
+        if (savedDegree === undefined) {
+            savedDegree = rotateDeg;
+            wheelBody.style.transform = `rotate(-${savedDegree}deg)`
+        } else {
+            savedDegree += rotateDeg;
+            wheelBody.style.transform = `rotate(-${savedDegree}deg)`
+        }
+    } catch (e) {
+        errorPopUp.textContent = e.message;
+        bet.style.border = '2px solid #ff3333';
+        errorPopUp.classList.add('goUp');
+    }
+
+}
+
+playBtn.addEventListener('transitionend', (e) => {
+    e.stopPropagation();
+})
+
+wheelBody.addEventListener('transitionend', () => {
+    getWinner();
+    wheelBody.classList.remove('blur');
+});
+
+function getWinner() {
+    const uppperElement = arrow.getBoundingClientRect();
+    const x = (uppperElement.right - uppperElement.left) / 2 + uppperElement.left;
+    const y = uppperElement.bottom;
+
+    const winnerSector = document.elementFromPoint(x, y);
+
+    const sectorAction = winnerSector.getAttribute('action');
+    const sectorValue = parseInt(winnerSector.getAttribute('value'));
+
+    scoreCalculation(sectorAction, sectorValue, newBet);
+    newBet = undefined;
+}
+
+
+function scoreCalculation(action, value, bet) {
+    let oldScore = parseInt(gameScore.textContent);
+    let betSum;
+
+    switch (action) {
+        case '+':
+            betSum = bet + value;
+            oldScore += betSum;
+
+            popUpValidation(betSum, popUp);
+            gameScore.textContent = oldScore;
+            break;
+        case '-':
+            betSum = bet - value;
+            oldScore += betSum;
+
+            popUpValidation(betSum, popUp);
+            gameScore.textContent = oldScore;
+            break;
+        case 'X':
+            betSum = bet * value;
+            oldScore += betSum;
+
+            popUpValidation(betSum, popUp);
+            gameScore.textContent = oldScore;
+            break;
+        case '/':
+            betSum = Math.floor(bet / value);
+            oldScore += betSum;
+
+            popUpValidation(betSum, popUp);
+            gameScore.textContent = oldScore;
+            break;
+    }
+}
+
+function popUpValidation(sum, popUp) {
+    if (sum > 0) {
+        popUp.textContent = `+ ${sum}$`;
+        popUp.style.color = '#5cb85c';
+    } else {
+        popUp.textContent = `${sum}$`;
+        popUp.style.color = '#ff3333';
+    }
+
+    popUp.classList.add('move-pop-up');
+}
+
+function inputValidation(betInput) {
+    if (betInput === '') {
+        throw new Error("The input is empty. Please type in your bet.");
+    } else if (betInput < 0) {
+        throw new Error("The input should be a positive number.");
+    }
+}
